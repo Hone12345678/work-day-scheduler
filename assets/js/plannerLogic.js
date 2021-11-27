3874df50132aff5d903138a2945689d9edabb00f
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2021-11-27 10:52:16 -0700

    revision to date format

--- assets/js/plannerLogic.js
+++ assets/js/plannerLogic.js
@@ -3,10 +3,10 @@ var todaysDateEl = document.querySelector("#currentDay");
 
 // start generate todays date start
 var currentDay = new Date();
-var today = (currentDay.getMonth()+1) + "/"
-    +currentDay.getDate() + "/"
-    +currentDay.getUTCFullYear();
-//display todays date 
-todaysDateEl.textContent = today;
-// end generate todays date end
+currentDay.toDateString();
+
+// display todays current date
+todaysDateEl.textContent = currentDay;
+// End generate todays date End
+
 
