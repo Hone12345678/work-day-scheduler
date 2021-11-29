48f8998acc9d0f32bff5c4188d198650bc4fccb0
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2021-11-28 19:33:54 -0700

    testing merge issue

---
+++ assets/js/localStorage.js
@@ -0,0 +1,15 @@
+
+var saveBtnEl = $('saveBtn');
+
+
+$(".saveBtn").on( "click", function(event) {
+
+    event.target;
+var inputEL= $(this).siblings("input").val();
+    console.log($(this).siblings("input").val());
+    console.log(inputEL)
+  });
+ 
+
+//   if save button is clicked add input element to the array
+
