07b655dce97a4e4990f6583304b73d90ab705a7b
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2021-11-29 19:14:21 -0700

    clocal storage for timeblocks and optimized app for mobile use using additional bootstrap

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -2,14 +2,30 @@
 var saveBtnEl = $('saveBtn');
 
 
-$(".saveBtn").on( "click", function(event) {
+$(".saveBtn").on("click", function (event) {
 
-    event.target;
-var inputEL= $(this).siblings("input").val();
-    console.log($(this).siblings("input").val());
-    console.log(inputEL)
-  });
- 
+  event.target;
+  var inputEL = $(this).siblings("input").val();
+  // console.log($(this).siblings("input").val());
+  // console.log(inputEL)
+
+  var time = $(this).parent().attr("id")
+  localStorage.setItem(time, inputEL)
+
+});
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"))
+$("#hour-10 .description").val(localStorage.getItem("hour-10"))
+$("#hour-11 .description").val(localStorage.getItem("hour-11"))
+$("#hour-12 .description").val(localStorage.getItem("hour-12"))
+$("#hour-13 .description").val(localStorage.getItem("hour-13"))
+$("#hour-14 .description").val(localStorage.getItem("hour-14"))
+$("#hour-15 .description").val(localStorage.getItem("hour-15"))
+$("#hour-16 .description").val(localStorage.getItem("hour-16"))
+$("#hour-17 .description").val(localStorage.getItem("hour-17"))
+
+
+
 
 //   if save button is clicked add input element to the array
 
